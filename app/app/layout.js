81d996dc1b897fe14c3398c8247f2d72b69d13c8
foreningslivetdev <124352225+foreningslivetdev@users.noreply.github.com>
2026-02-09 22:51:59 +0100

    Add metadata for Spotadive in layout.js

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "Spotadive",
+  description: "Öppen statistik om diving-utvisningar i hockey",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sv">
+      <body>{children}</body>
+    </html>
+  );
+}
 
